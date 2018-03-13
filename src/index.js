c55d305c2a20c4a0d698a5584b2876ceea3199af
Octave Raimbault <dispyx@gmail.com>
2018-03-13 15:55:51 +0100

    Subscribe returns an unsubscribe function

--- src/index.js
+++ src/index.js
@@ -1,53 +1,57 @@
 // @flow
-import React, { Component, createContext } from 'react'
-import shallowEqual from 'fbjs/lib/shallowEqual'
+import React, { Component, createContext } from "react";
+import shallowEqual from "fbjs/lib/shallowEqual";
 
-const err = () => console.error('Provider is not initialized yet')
+const err = () => console.error("Provider is not initialized yet");
 
 class Prevent extends Component<*> {
   shouldComponentUpdate = ({ state, select }) =>
     select.some(
-      selector => !shallowEqual(this.props.state[selector], state[selector]),
-    )
+      selector => !shallowEqual(this.props.state[selector], state[selector])
+    );
 
   render() {
-    const { actions, select, state, children } = this.props
-    const selected = select.reduce((r, v) => ({ ...r, [v]: state[v] }), {})
-    return children({ state: selected, actions })
+    const { actions, select, state, children } = this.props;
+    const selected = select.reduce((r, v) => ({ ...r, [v]: state[v] }), {});
+    return children({ state: selected, actions });
   }
 }
 
 export const initStore: Function = (store, ...middlewares) => {
-  let self, initializedMiddlewares
-  let subscriptions = []
-  const Context = createContext()
+  let self, initializedMiddlewares;
+  let subscriptions = [];
+  const Context = createContext();
 
-  const getState = () => (self ? self.state : err())
+  const getState = () => (self ? self.state : err());
   const setState = (action, state) => {
-    subscriptions.forEach(fn => fn(action, state))
-    self.setState(state, () => initializedMiddlewares.forEach(m => m(action)))
-  }
+    subscriptions.forEach(fn => fn(action, state));
+    self.setState(state, () => initializedMiddlewares.forEach(m => m(action)));
+  };
 
   const subscribe = fn => {
-    subscriptions = [...subscriptions, fn]
-  }
+    subscriptions = [...subscriptions, fn];
+
+    return function unsubscribe() {
+      subscriptions.splice(subscriptions.indexOf(fn), 1);
+    };
+  };
 
   const actions = Object.keys(store.actions).reduce(
     (r, v) => ({
       ...r,
       [v]: (...args) => {
         if (self) {
-          let result = store.actions[v](self.state, ...args)
+          let result = store.actions[v](self.state, ...args);
           result.then
             ? result.then(result => setState(v, result))
-            : setState(v, result)
+            : setState(v, result);
         } else {
-          err()
+          err();
         }
-      },
+      }
     }),
-    {},
-  )
+    {}
+  );
 
   const Consumer = ({ children, select }) => (
     <Context.Consumer>
@@ -57,22 +61,22 @@ export const initStore: Function = (store, ...middlewares) => {
         </Prevent>
       )}
     </Context.Consumer>
-  )
+  );
 
   const connect = select => Cmpnt => props => {
     return (
       <Consumer select={select}>
         {({ state }) => <Cmpnt {...props} state={state} actions={actions} />}
       </Consumer>
-    )
-  }
+    );
+  };
 
   class Provider extends Component<*> {
     constructor() {
-      super()
-      self = this
-      this.state = store.initialState
-      initializedMiddlewares = middlewares.map(m => m(store, self))
+      super();
+      self = this;
+      this.state = store.initialState;
+      initializedMiddlewares = middlewares.map(m => m(store, self));
     }
 
     render() {
@@ -80,12 +84,12 @@ export const initStore: Function = (store, ...middlewares) => {
         <Context.Provider
           value={{
             state: this.state,
-            actions,
+            actions
           }}
         >
           {this.props.children}
         </Context.Provider>
-      )
+      );
     }
   }
 
@@ -95,6 +99,6 @@ export const initStore: Function = (store, ...middlewares) => {
     actions,
     getState,
     connect,
-    subscribe,
-  }
-}
+    subscribe
+  };
+};
